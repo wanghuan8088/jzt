f342543f58a977931b4f3172c6a369e2c16b5aba
kavinhub <kavinhub@163.com>
2017-03-11 16:47:33 +0800

    Add DAO for product.

---
+++ jzt-api/src/java/com/jzt/api/domain/Product.java
@@ -0,0 +1,95 @@
+package com.jzt.api.domain;
+
+import java.util.Date;
+
+public class Product {
+    private Integer id;
+
+    private String name;
+
+    private Integer type;
+
+    private Integer marketPrice;
+
+    private Integer price;
+
+    private String description;
+
+    private Integer status;
+
+    private Date createtime;
+
+    private Date modifytime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public Integer getMarketPrice() {
+        return marketPrice;
+    }
+
+    public void setMarketPrice(Integer marketPrice) {
+        this.marketPrice = marketPrice;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public Date getModifytime() {
+        return modifytime;
+    }
+
+    public void setModifytime(Date modifytime) {
+        this.modifytime = modifytime;
+    }
+}
