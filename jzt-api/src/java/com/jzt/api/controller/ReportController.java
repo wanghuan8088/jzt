c2afa80d581824212749eea297c7327eccaa1bd2
kavinhub <kavinhub@163.com>
2017-03-12 01:42:39 +0800

    update for my fav report list.

--- jzt-api/src/java/com/jzt/api/controller/ReportController.java
+++ jzt-api/src/java/com/jzt/api/controller/ReportController.java
@@ -1,51 +1,140 @@
 package com.jzt.api.controller;
 
-import com.jzt.api.controller.base.BaseController;
-import com.jzt.api.domain.Report;
-import com.jzt.api.service.ReportService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-/**
- * Created by wanghuan on 10/03/2017.
- */
+import net.sf.json.JSONObject;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.jzt.api.controller.base.BaseController;
+import com.jzt.api.domain.Report;
+import com.jzt.api.domain.UserInformation;
+import com.jzt.api.service.ReportService;
+
+/**
+ * Desc: ReportController
+ * 
+ * @author Kavin
+ * @since  2016-Sep-7 
+ */
 @Controller
 @RequestMapping("/rest/{version}/report")
 public class ReportController extends BaseController {
-
-    @Autowired
-    private ReportService reportService;
-
-
-    @RequestMapping(value = "/list/{date}")
-    @ResponseBody
-    public Map<String, Object> list(@PathVariable String date){
-
-        Map<String, Object> result = new HashMap<String, Object>();
-
-        try {
-            Map<String, Object> data = new HashMap<String, Object>();
-            Report report = new Report();
-            List<Report> list = reportService.list(report);
-
-            data.put("report", list);
-            result.put("data", data );
-            result.put("res", "0");
-            result.put("message", "Success");
-        } catch (Exception e) {
-            result.put("res", "1");
-            result.put("message", "Error-"+e.getMessage());
-        }
-
-        return result;
-    }
+	
+	@Autowired
+	private ReportService reportService;
+	
+	/**  
+	* @Title: save  
+	* @Description: save
+	* @param @param para
+	* @param @return    设定文件  
+	* @return Map<String,Object>    返回类型  
+	* @throws  
+	*/
+	@RequestMapping(value = "/save")
+	@ResponseBody
+	public Map<String, Object> save(@RequestParam(value="para", required=true) String para){
+		JSONObject jsStr = JSONObject.fromObject(para);
+		Report record = (Report) JSONObject.toBean(jsStr, Report.class);
+		Map<String, Object> result = new HashMap<String, Object>();
+		
+		try {
+			result = reportService.save(record);
+		} catch (Exception e) {
+			result = generateErrorResult(e);
+		}
+		return result;
+	}
+	
+	/**  
+	* @Title: saveOrUpdate  
+	* @Description: saveOrUpdate
+	* @param @param para
+	* @param @return    设定文件  
+	* @return Map<String,Object>    返回类型  
+	* @throws  
+	*/
+	@RequestMapping(value = "/saveOrUpdate")
+	@ResponseBody
+	public Map<String, Object> saveOrUpdate(@RequestParam(value="para", required=true) String para){
+		JSONObject jsStr = JSONObject.fromObject(para);
+		Report record = (Report) JSONObject.toBean(jsStr, Report.class);
+		Map<String, Object> result = new HashMap<String, Object>();
+		
+		try {
+			result = reportService.saveOrUpdate(record);
+		} catch (Exception e) {
+			result = generateErrorResult(e);
+		}
+		return result;
+	}
+	
+	/**  
+	* @Title: delete  
+	* @Description: delete 
+	* @param @param para
+	* @param @return    设定文件  
+	* @return Map<String,Object>    返回类型  
+	* @throws  
+	*/
+	@RequestMapping(value = "/delete")
+	@ResponseBody
+	public Map<String, Object> delete(@RequestParam(value="para", required=true) String para){
+		JSONObject jsStr = JSONObject.fromObject(para);
+		Report record = (Report) JSONObject.toBean(jsStr, Report.class);
+		Map<String, Object> result = new HashMap<String, Object>();
+		
+		try {
+			result = reportService.delete(record);
+		} catch (Exception e) {
+			result = generateErrorResult(e);
+		}
+		return result;
+	}
+	
+	/**  
+	* @Title: query  
+	* @Description: query 
+	* @param @param para
+	* @param @return    设定文件  
+	* @return Map<String,Object>    返回类型  
+	* @throws  
+	*/
+	@RequestMapping(value = "/query")
+	@ResponseBody
+	public Map<String, Object> query(@RequestParam(value="para", required=true) String para){
+		JSONObject jsStr = JSONObject.fromObject(para);
+		Report record = (Report) JSONObject.toBean(jsStr, Report.class);
+		Map<String, Object> result = new HashMap<String, Object>();
+		
+		try {
+			result = reportService.selectOneByPrimaryKey(record.getId());
+		} catch (Exception e) {
+			result = generateErrorResult(e);
+		}
+		return result;
+	}
+	
+	@RequestMapping(value = "/selectReportsByUid")
+	@ResponseBody
+	public Map<String, Object> selectReportsByUid(@RequestParam(value="para", required=true) String para){
+		JSONObject jsStr = JSONObject.fromObject(para);
+		UserInformation record = (UserInformation) JSONObject.toBean(jsStr, UserInformation.class);
+		Map<String, Object> result = new HashMap<String, Object>();
+		
+		try {
+			result = reportService.selectReportsByUid(record.getUid());
+		} catch (Exception e) {
+			result = generateErrorResult(e);
+		}
+		return result;
+	}
+	
 
 }
--- jzt-api/src/java/com/jzt/api/service/impl/ReportServiceImpl.java
+++ jzt-api/src/java/com/jzt/api/service/impl/ReportServiceImpl.java
@@ -1,25 +1,231 @@
 package com.jzt.api.service.impl;
 
-import com.jzt.api.dao.ReportMapper;
-import com.jzt.api.domain.Report;
-import com.jzt.api.service.ReportService;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.jzt.api.dao.ReportMapper;
+import com.jzt.api.domain.Report;
+import com.jzt.api.domain.ReportExample;
+import com.jzt.api.service.ReportService;
 
-/**
- * Created by wanghuan on 10/03/2017.
- */
 
+/** 
+* @ClassName: ReportServiceImpl 
+* @Description: ReportServiceImpl class
+* @author  Kavin
+* @date 2017年3月10日 上午9:35:55 
+*  
+*/
 @Service
-public class ReportServiceImpl implements ReportService{
+public class ReportServiceImpl extends BaseService implements ReportService {
 
-    @Autowired
-    private ReportMapper reportMapper;
+	@Autowired
+	private ReportMapper reportMapper;
+	
+	/* (非 Javadoc)  
+	* <p>Title: save</p>  
+	* <p>Description: </p>  
+	* @param record
+	* @return  
+	* @see com.jzt.api.service.ReportService#save(com.jzt.api.domain.Report)  
+	*/
+	@Override
+	public Map<String, Object> save(Report record) {
+		
+		Map<String, Object> result = new HashMap<String, Object>();
+		try {
+			reportMapper.insertSelective(record);
+			result = generateNomalResult(record);
+		} catch (Exception e) {
+			result = generateErrorResult(e);
+		}
+		return result;
+	}
 
+	/* (非 Javadoc)  
+	* <p>Title: delete</p>  
+	* <p>Description: </p>  
+	* @param record
+	* @return  
+	* @see com.jzt.api.service.ReportService#delete(com.jzt.api.domain.Report)  
+	*/
+	@Override
+	public Map<String, Object> delete(Report record) {
+		
+		Map<String, Object> result = new HashMap<String, Object>();
+		try {
+			reportMapper.deleteByPrimaryKey(record.getId());
+			result = generateNomalResult(record);
+		} catch (Exception e) {
+			result = generateErrorResult(e);
+		}
+		return result;
+	}
+
+	
+
+	/* (非 Javadoc)  
+	* <p>Title: selectOneByPrimaryKey</p>  
+	* <p>Description: </p>  
+	* @param id
+	* @return  
+	* @see com.jzt.api.service.ReportService#selectOneByPrimaryKey(java.lang.Integer)  
+	*/
+	@Override
+	public Map<String, Object> selectOneByPrimaryKey(Integer id) {
+		Map<String, Object> result = new HashMap<String, Object>();
+		try {
+			Report record = reportMapper.selectByPrimaryKey(id);
+			if(record!=null ){
+				result = generateNomalResult(record);
+			}
+		} catch (Exception e) {
+			result = generateErrorResult(e);
+		}
+		
+		return result;
+	}
+
+	/* (非 Javadoc)  
+	* <p>Title: saveOrUpdate</p>  
+	* <p>Description: </p>  
+	* @param record
+	* @return  
+	* @see com.jzt.api.service.ReportService#saveOrUpdate(com.jzt.api.domain.Report)  
+	*/
+	@Override
+	public Map<String, Object> saveOrUpdate(Report record) {
+		
+		Map<String, Object> result = new HashMap<String, Object>();
+		try {
+			
+			if(record.getId()!=null && record.getId()!=0){
+				
+				Report dto = reportMapper.selectByPrimaryKey(record.getId());
+				
+				if(dto!=null ){
+					reportMapper.updateByPrimaryKeySelective(record);
+				}else{
+					reportMapper.insertSelective(record);
+				}
+			}else{
+				reportMapper.insertSelective(record);
+			}
+			
+			result = generateNomalResult(record);
+			
+		} catch (Exception e) {
+			result = generateErrorResult(e);
+		}
+		return result;
+	}
+	
+	/* (非 Javadoc)  
+	* <p>Title: queryListByPage</p>  
+	* <p>Description: </p>  
+	* @param record
+	* @return  
+	* @see com.jzt.api.service.ReportService#queryListByPage(com.jzt.api.domain.Report)  
+	*/
+	@Override
+	public Map<String, Object> queryListByPage(Report record) {
+		Map<String, Object> result = new HashMap<String, Object>();
+		try {
+			ReportExample example = new ReportExample();
+//			setOrderByClause(record, example);
+			List<Report> list = reportMapper.selectByExample(example);
+			if(list!=null ){
+				result = generateNomalResult(list);
+			}
+		} catch (Exception e) {
+			result = generateErrorResult(e);
+		}
+		
+		return result;
+	}
+	
+	
+	@Override
+	public int countByExample(ReportExample example){
+		return reportMapper.countByExample(example);
+	}
+
+	@Override
+	public int deleteByExample(ReportExample example){
+		return reportMapper.deleteByExample(example);
+	}
+    
+	@Override
+	public int deleteByPrimaryKey(Integer id) {
+		return reportMapper.deleteByPrimaryKey(id);
+	}
+    
     @Override
-    public List<Report> list(Report report) {
-        return reportMapper.selectByExample(null);
-    }
+    public int insert(Report record) {
+		return reportMapper.insert(record);
+	}
+
+	@Override
+    public int insertSelective(Report record) {
+		return reportMapper.insertSelective(record);
+	}
+
+	@Override
+	public List<Report> selectByExample(ReportExample example) {
+		return reportMapper.selectByExample(example);
+	}
+
+	@Override
+    public Report selectByPrimaryKey(Integer id) {
+		return reportMapper.selectByPrimaryKey(id);
+	}
+
+	@Override
+	public int updateByExampleSelective(@Param("record") Report record, @Param("example") ReportExample example) {
+		return reportMapper.updateByExampleSelective(record, example);
+	}
+
+	@Override
+	public int updateByExample(@Param("record") Report record, @Param("example") ReportExample example) {
+		return reportMapper.updateByExample(record, example);
+	}
+
+	@Override
+	public int updateByPrimaryKeySelective(Report record) {
+		return reportMapper.updateByPrimaryKeySelective(record);
+	}
+
+	@Override
+	public int updateByPrimaryKey(Report record) {
+		return reportMapper.updateByPrimaryKey(record);
+	}
+
+	/* (非 Javadoc)  
+	* <p>Title: selectReportsByUid</p>  
+	* <p>Description: </p>  
+	* @param uid
+	* @return  
+	* @see com.jzt.api.service.ReportService#selectReportsByUid(int)  
+	*/
+	@Override
+	public Map<String, Object> selectReportsByUid(int uid) {
+		Map<String, Object> result = new HashMap<String, Object>();
+		try {
+			ReportExample example = new ReportExample();
+			List<Report> list = reportMapper.selectReportsByUid(uid);
+			if(list!=null ){
+				result = generateNomalResult(list);
+			}
+		} catch (Exception e) {
+			result = generateErrorResult(e);
+		}
+		
+		return result;
+	}
+	
 }
