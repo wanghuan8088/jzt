cced35fdcab714b920c2038526b2eb8f6fa5641d
kavinhub <kavinhub@163.com>
2017-03-26 09:26:02 +0800

    Add for message_info API.

---
+++ jzt-api/src/java/com/jzt/api/service/MessageInfoService.java
@@ -0,0 +1,90 @@
+package com.jzt.api.service;
+
+import java.util.List;
+import java.util.Map;
+import org.apache.ibatis.annotations.Param;
+import com.jzt.api.domain.MessageInfo;
+import com.jzt.api.domain.MessageInfoExample;
+
+/**
+ * Desc: MessageInfoService
+ * 
+ * @author Kavin
+ * @since  2017-Mar-6 
+ */
+public interface MessageInfoService {
+
+	/**  
+	* @Title: save  
+	* @Description: save 
+	* @param @param record
+	* @param @return    设定文件  
+	* @return Map<String,Object>    返回类型  
+	* @throws  
+	*/
+	Map<String, Object> save(MessageInfo record);
+
+	/**  
+	* @Title: delete  
+	* @Description: delete 
+	* @param @param record
+	* @param @return    设定文件  
+	* @return Map<String,Object>    返回类型  
+	* @throws  
+	*/
+	Map<String, Object> delete(MessageInfo record);
+
+	/**  
+	* @Title: selectOneByPrimaryKey  
+	* @Description: selectOneByPrimaryKey 
+	* @param @param id
+	* @param @return    设定文件  
+	* @return Map<String,Object>    返回类型  
+	* @throws  
+	*/
+	Map<String, Object> selectOneByPrimaryKey(Integer id);
+
+	/**  
+	* @Title: saveOrUpdate  
+	* @Description: saveOrUpdate 
+	* @param @param record
+	* @param @return    设定文件  
+	* @return Map<String,Object>    返回类型  
+	* @throws  
+	*/
+	Map<String, Object> saveOrUpdate(MessageInfo record);
+	
+	/**  
+	* @Title: queryListByPage  
+	* @Description: queryListByPage 
+	* @param @param record
+	* @param @return    设定文件  
+	* @return Map<String,Object>    返回类型  
+	* @throws  
+	*/
+	Map<String, Object> queryListByPage(MessageInfo record);
+	
+	
+	int countByExample(MessageInfoExample example);
+
+    int deleteByExample(MessageInfoExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(MessageInfo record);
+
+    int insertSelective(MessageInfo record);
+
+    List<MessageInfo> selectByExample(MessageInfoExample example);
+
+    MessageInfo selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") MessageInfo record, @Param("example") MessageInfoExample example);
+
+    int updateByExample(@Param("record") MessageInfo record, @Param("example") MessageInfoExample example);
+
+    int updateByPrimaryKeySelective(MessageInfo record);
+
+    int updateByPrimaryKey(MessageInfo record);
+	
+}
