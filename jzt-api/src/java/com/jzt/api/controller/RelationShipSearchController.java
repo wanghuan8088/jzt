46636abe235cf9e14bfc69cd22f1cfefcc2b0d0d
wanghuan <wanghuan8086@163.com>
2017-04-20 02:33:33 +0800

    关系搜索

--- jzt-api/src/java/com/jzt/api/controller/RelationShipSearchController.java
+++ jzt-api/src/java/com/jzt/api/controller/RelationShipSearchController.java
@@ -1,32 +1,22 @@
 package com.jzt.api.controller;
 
-import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import com.jzt.api.controller.base.BaseController;
+import com.jzt.api.domain.*;
+import com.jzt.api.service.CompanyService;
+import com.jzt.api.service.PlatformService;
+import com.jzt.api.service.SearchService;
 import net.sf.json.JSONObject;
-
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.jzt.api.controller.base.BaseController;
-import com.jzt.api.domain.BankProduct;
-import com.jzt.api.domain.Businessman;
-import com.jzt.api.domain.Company;
-import com.jzt.api.domain.P2pLoan;
-import com.jzt.api.domain.Platform;
-import com.jzt.api.domain.Search;
-import com.jzt.api.domain.Stockholder;
-import com.jzt.api.service.CompanyService;
-import com.jzt.api.service.PlatformService;
-import com.jzt.api.service.SearchService;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  *关系搜索接口 
@@ -121,13 +111,13 @@ public class RelationShipSearchController extends BaseController {
     /**
      * 关系搜索 -关联关系
      * 人 VS 人，人 VS 平台， 平台 VS 平台
-     * @param pid1
-     * @param pid2
      * @return
      */
-    @RequestMapping(value = "/relationship/{rs1}/{rs2}")
+    @RequestMapping(method = RequestMethod.POST, value = "/relationship")
     @ResponseBody
-    public Map<String, Object> relationship(@PathVariable(value="rs1") Integer rs1, @PathVariable(value="rs2") Integer rs2){
+    public Map<String, Object> relationship(@RequestParam(value = "para", required = true) String para) {
+        JSONObject jsStr = JSONObject.fromObject(para);
+        //Search dto = (Search) JSONObject.toBean(jsStr, Search.class);
 
         Map<String, Object> result = new HashMap<String, Object>();
 
@@ -135,14 +125,14 @@ public class RelationShipSearchController extends BaseController {
             Map<String, Object> data = new HashMap<String, Object>();
             //第一步先进行A平台查询
 			Platform pa = new Platform();
-			pa.setId(rs1);
+			//pa.setId(rs1);
 			Platform platformA = platformService.detail(pa);
 			if(platformA!=null){
 				 data.put("platformA", platformA);
 			}
             //对第二个关键字进行平台查询
 			Platform pb = new Platform();
-			pb.setId(rs2);
+			//pb.setId(rs2);
 			Platform platformB = platformService.detail(pb);
 			if(platformB!=null){
 				 data.put("platformB", platformB);
@@ -150,15 +140,71 @@ public class RelationShipSearchController extends BaseController {
 				//如果平台为null,则无平台信息;分别遍历公司法人，股东和高管信息
 				//step1 获取公司法人信息
 				Company cb = new Company();
-				cb.setId(rs2);
+				//cb.setId(rs2);
 				Company companyB = companyService.detail(cb);
 				if(companyB!=null){
-					data.put("companyB", companyB);
+					//data.put("companyB", companyB);
 				}
 				
 			}
-         
-            
+
+
+            Map<String, Object> people_VS_platform = new HashMap<String, Object>();
+            people_VS_platform.put("platform_id","4");
+            people_VS_platform.put("icon","https://assets-cdn.github.com/favicon.ico");
+            people_VS_platform.put("platform_name","支付宝");
+            people_VS_platform.put("company_name","支付宝网络技术有限公司");
+            people_VS_platform.put("artificial_person","马云");
+            people_VS_platform.put("stockholder","马云");
+            people_VS_platform.put("businessman","马云");
+            data.put("people_VS_platform", people_VS_platform);
+
+
+            List people_VS_people = new ArrayList();
+            Map<String, Object> people_VS_people1 = new HashMap<String, Object>();
+            people_VS_people1.put("platform_id","5");
+            people_VS_people1.put("icon","https://assets-cdn.github.com/favicon.ico");
+            people_VS_people1.put("platform_name","支付宝xxxxx");
+            people_VS_people1.put("company_name","支付宝网络技术有限公司");
+            people_VS_people1.put("artificial_person","马云");
+            people_VS_people1.put("stockholder","马化腾");
+            people_VS_people1.put("businessman","马化腾");
+            Map<String, Object> people_VS_people2 = new HashMap<String, Object>();
+            people_VS_people2.put("platform_id","6");
+            people_VS_people2.put("icon","https://assets-cdn.github.com/favicon.ico");
+            people_VS_people2.put("platform_name","点点搜财");
+            people_VS_people2.put("company_name","点点搜财有限公司");
+            people_VS_people2.put("artificial_person","马云");
+            people_VS_people2.put("stockholder","马化腾");
+            people_VS_people2.put("businessman","马云");
+
+            people_VS_people.add(people_VS_people1);
+            people_VS_people.add(people_VS_people2);
+            data.put("people_VS_people", people_VS_people);
+
+
+            List platform_VS_platform = new ArrayList();
+            Map<String, Object> platform_VS_platform1 = new HashMap<String, Object>();
+            platform_VS_platform1.put("platform_id","5");
+            platform_VS_platform1.put("icon","https://assets-cdn.github.com/favicon.ico");
+            platform_VS_platform1.put("platform_name","支付宝xxxxx");
+            platform_VS_platform1.put("company_name","支付宝网络技术有限公司");
+            platform_VS_platform1.put("artificial_person","马云");
+            platform_VS_platform1.put("stockholder","马化腾");
+            platform_VS_platform1.put("businessman","李彦宏");
+            Map<String, Object> platform_VS_platform2 = new HashMap<String, Object>();
+            platform_VS_platform2.put("platform_id","6");
+            platform_VS_platform2.put("icon","https://assets-cdn.github.com/favicon.ico");
+            platform_VS_platform2.put("platform_name","点点搜财");
+            platform_VS_platform2.put("company_name","点点搜财有限公司");
+            platform_VS_platform2.put("artificial_person","马云");
+            platform_VS_platform2.put("stockholder","马化腾");
+            platform_VS_platform2.put("businessman","李彦宏");
+
+            platform_VS_platform.add(platform_VS_platform1);
+            platform_VS_platform.add(platform_VS_platform2);
+            data.put("platform_VS_platform", platform_VS_platform);
+
 
             result.put("data", data );
             result.put("res", "0");
